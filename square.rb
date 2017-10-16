8b8f3f6c2e81fc25f04db1b4b713d94a07e58003
mnash <utumno86@gmail.com>
2017-10-16 04:50:47 -0400

    fun with getters and setters

--- square.rb
+++ square.rb
@@ -3,6 +3,16 @@ class Square
   def initialize(side_length)
     @side_length = side_length
   end
+
+  def side_length
+    @side_length
+  end
+
+  def side_length=(side_length)
+    @side_length = side_length
+  end
 end
 my_square = Square.new(20)
-puts my_square.inspect
+puts my_square.side_length
+my_square.side_length = 10
+puts my_square.side_length
