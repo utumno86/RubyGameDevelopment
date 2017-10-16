b912382739bf312d61310a132dc9bb7bc6f7ac23
mnash <utumno86@gmail.com>
2017-10-16 05:05:42 -0400

    fun with object to_s

--- square.rb
+++ square.rb
@@ -11,8 +11,22 @@ class Square
   def side_length=(side_length)
     @side_length = side_length
   end
+
+  def perimeter
+    @side_length * 4
+  end
+
+  def area
+    @side_length**2
+  end
+
+  def to_s
+    "Side Length: #{@side_length}\nPerimeter: #{perimeter}\nArea: #{area}"
+  end
 end
 my_square = Square.new(20)
 puts my_square.side_length
 my_square.side_length = 10
 puts my_square.side_length
+
+puts my_square
