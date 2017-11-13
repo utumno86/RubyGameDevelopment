f518d3d89b94be9ffe3f6b162e9d601600442780
mnash <utumno86@gmail.com>
2017-11-12 21:48:04 -0500

    added media and a background

--- space.rb
+++ space.rb
@@ -3,14 +3,18 @@ require 'gosu'
 class Tutorial < Gosu::Window
   def initialize
     super 640, 480
-    self.caption = "My Awesome Gosu Tutorial"
+    self.caption = 'My Awesome Gosu Tutorial'
+
+    @background_image = Gosu::Image.new('media/space.png', :tileable => true)
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
+  
 end
 
 Tutorial.new.show
