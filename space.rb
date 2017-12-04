df0c142a6cd890cd7c8b306fdc0d2cafac6a022b
mnash <utumno86@gmail.com>
2017-12-03 21:15:03 -0500

    created star class

--- space.rb
+++ space.rb
@@ -81,4 +81,29 @@ class Player
   end
 end
 
+# Do some ZOrder constant stuff
+module ZOrder
+  BACKGROUND, STARS, PLAYER, UI = *0..3
+end
+
+#Add some big star animations
+
+class Star
+  attr_reader :x, :y
+
+  def initialize
+    @animation = animation
+    @color = Gosu::Color::BLACK.dup
+    @color.red = rand(256 - 40) + 40
+    @color.green = rand(256 - 40) + 40
+    @x = rand * 640
+    @y = rand * 480
+  end
+
+  def draw
+    img = @animation[Gosu.miliseconds / 100 % @animation.size]
+    img.draw(@x - img.width / 2.0, @y - img.height / 2.0, ZOrder::STARS, 1, 1, @color, :add)
+  end
+end
+
 Tutorial.new.show
