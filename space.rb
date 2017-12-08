d439f660942c07d7220da9dc7844c364246a555d
mnash <utumno86@gmail.com>
2017-12-07 22:10:39 -0500

    finished the space game

--- space.rb
+++ space.rb
@@ -17,6 +17,8 @@ class Tutorial < Gosu::Window
 
     @star_anim = Gosu::Image.load_tiles('media/star.png', 25, 25)
     @stars = []
+
+    @font = Gosu::Font.new(20)
   end
 
   def update
@@ -43,6 +45,7 @@ class Tutorial < Gosu::Window
     @player.draw
     @background_image.draw(0, 0, ZOrder::BACKGROUND)
     @stars.each { |star| star.draw }
+    @font.draw("Score #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
   end
 
   def button_down(id)
@@ -56,8 +59,11 @@ end
 
 # Our Main Player Class
 class Player
+  attr_reader :score
+
   def initialize
     @image = Gosu::Image.new('media/starfighter.bmp')
+    @beep = Gosu::Sample.new('media/beep.wav')
     @x = @y = @vel_x = @vel_y = @angle = 0.0
     @score = 0
   end
@@ -98,9 +104,16 @@ class Player
   end
 
   def collect_stars(stars)
-    stars.reject! { |star| Gosu.distance(@x, @y, star.x, star.y) < 35 }
+    stars.reject! do |star|
+      if Gosu.distance(@x, @y, star.x, star.y) < 35
+        @score += 10
+        @beep.play
+        true
+      else
+        false
+      end
+    end
   end
-
 end
 
 # Add some big star animations
