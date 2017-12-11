f0d9e3926ef13f167370e0933714c3a199daafd4
mnash <utumno86@gmail.com>
2017-12-10 21:42:19 -0500

    added function to make the mole blink in and out of existence

--- mole.rb
+++ mole.rb
@@ -11,10 +11,13 @@ class WhackaMole < Gosu::Window
     @height = 75
     @velocity_x = 5
     @velocity_y = 5
+    @visible = 0
   end
 
   def draw
-    @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    end
   end
 
   def update
@@ -22,6 +25,8 @@ class WhackaMole < Gosu::Window
     @y += @velocity_y
     @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
     @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
 
 
