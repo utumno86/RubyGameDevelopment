83711df4b16c95fcfa62a94a0115d8d89f393617
mnash <utumno86@gmail.com>
2017-12-10 21:46:35 -0500

    animated the mole hammer

--- mole.rb
+++ mole.rb
@@ -4,7 +4,8 @@ class WhackaMole < Gosu::Window
   def initialize
     super(800,600)
     self.caption = "Wack a Mole"
-    @image = Gosu::Image.new('media/mole.png')
+    @mole = Gosu::Image.new('media/mole.png')
+    @hammer = Gosu::Image.new('media/hammer.png')
     @x = 200
     @y = 200
     @width = 100
@@ -12,12 +13,14 @@ class WhackaMole < Gosu::Window
     @velocity_x = 5
     @velocity_y = 5
     @visible = 0
+    @hit = 0
   end
 
   def draw
     if @visible > 0
-      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+      @mole.draw(@x - @width / 2, @y - @height / 2, 1)
     end
+    @hammer.draw(mouse_x - 25, mouse_y - 39, 1)
   end
 
   def update
@@ -27,6 +30,9 @@ class WhackaMole < Gosu::Window
     @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
     @visible -= 1
     @visible = 30 if @visible < -10 && rand < 0.01
+  end
+
+  def button_down(id)
   end
 
 
